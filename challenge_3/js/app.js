261574b2f2d43013376ce14f0e9bc957f946dfe2
AlexiOHearn <alexiohearn@o.local>
2017-05-03 16:23:25 -0600

    gets loan calc

--- challenge_3/js/app.js
+++ challenge_3/js/app.js
@@ -1,9 +1,25 @@
 console.log('challenge #3 -- app.js loaded!');
 
-var app = angular.module("appThree", []);
-app.controller("infoController", infoController);
+var app = angular.module("appThree", [])
+	.controller("infoController", infoController)
+	.controller("loanController", loanController);
 
 function infoController(){
   this.name = "Jon Doe"; // default name
   this.height = 5.6; // default height in feet
+}
+
+function loanController(){
+	this.amount = 0;
+	this.apr = 0;
+	this.month = function(n){
+		var yearInterest = (this.amount * (this.apr/100));
+		var monthInterest = (yearInterest/12);
+		var accrued_interest = Math.round((n * monthInterest)*100)/100;
+		var total_balance = Math.round((this.amount + accrued_interest)*100)/100;
+		return {
+			accrued_interest,
+			total_balance
+		};
+	};
 }
